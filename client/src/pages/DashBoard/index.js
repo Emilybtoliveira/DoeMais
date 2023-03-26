6d09e75ae32f7714e3ea73accae7143e6a87c15c
thaliabma <75902816+thaliabma@users.noreply.github.com>
2023-03-26 20:32:05 -0300

    Implementando feed principal

--- client/src/pages/DashBoard/index.js
+++ client/src/pages/DashBoard/index.js
@@ -2,16 +2,13 @@ import React from 'react'
 import {Container} from './styles'
 import Header from '../../components/Header'
 import FormCadastro from '../../components/FormCadastro'
-
+import Feed from '../../components/Feed'
 
 function Cadastro (){
     
     return(
         <div style={{overflowX: 'hidden'}}>
-        <Header/>
-        <Container>
-            <h1>LOGADA!</h1>  
-        </Container>
+        <Feed/>
         </div>
         
     )
