1f2c132f451db4badfd0d457b16e688efa1e3167
thaliabma <75902816+thaliabma@users.noreply.github.com>
2023-05-04 11:05:57 -0300

    Melhorando a UX da tela do mapa

--- client/src/components/Feed/LocalDoacao/index.js
+++ client/src/components/Feed/LocalDoacao/index.js
@@ -1,10 +1,10 @@
 import React, {  useEffect, useState  } from 'react'
 import {Grid}from '@mui/material';
-import {Container} from './styles'
+import {Container, CardLocal} from './styles'
 import clinica from '../../../assets/Feed/clinica.svg'
 import {useSelector} from 'react-redux'
 import { Card, CardContent, Typography } from '@mui/material';
-
+import pin from '../../../assets/location-pin.png'
 import {Link} from 'react-router-dom'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import BloodtypeIcon from '@mui/icons-material/Bloodtype';  
@@ -13,7 +13,19 @@ export default function Mapa () {
   const [bloodBanks, setBloodBanks] = useState([]);
   const location = useSelector(state => state.user.location);
   const cidade = useSelector(state => state.user.cidade);
-  
+  const [lat, setLat] = useState(location.latitude);
+  const [lng, setLng] = useState(location.longitude);
+  const [isActive, setIsActive] = useState(false);
+
+
+  const handleCardClick = (local) => {
+    setIsActive(!isActive);
+    setLat(local.lat)
+    setLng(local.lng)
+    
+  };
+
+
   const consultaBancoSangue = async () => {
     const map = new window.google.maps.Map(document.createElement("div"));
     
@@ -48,32 +60,31 @@ export default function Mapa () {
   
 
 
-  
-const BloodBankInfoCard = ({ name, address, opening_hours }) => (
-  <Card variant="outlined">
-    <CardContent>
-      {console.log(opening_hours?.open_now)}
-      <Typography variant="h5" component="h2">
-        {name}
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        {address}
-      </Typography>
-        {opening_hours?opening_hours.open_now?  
-        <Typography variant="body2" color='green' >
-          Aberto
+    
+  const BloodBankInfoCard = ({ name, address, opening_hours, local }) => (
+    <CardLocal variant="outlined" onClick={() => handleCardClick(local)}  sx={{cursor: 'pointer'}}>
+      <CardContent className='content' >
+        <Typography variant="h5" component="h2">
+          {name}
         </Typography>
-        :
-        <Typography variant="body2" color='red'>
-          Fechado
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {address}
         </Typography>
-        :
-        <Typography variant="body2" color='blue'>
-          Não Informado
-      </Typography>}
-    </CardContent>
-  </Card>
-);
+          {opening_hours?opening_hours.open_now?  
+          <Typography variant="body2" color='green' >
+            Aberto
+          </Typography>
+          :
+          <Typography variant="body2" color='red'>
+            Fechado
+          </Typography>
+          :
+          <Typography variant="body2" color='blue'>
+            Não Informado
+        </Typography>}
+      </CardContent>
+    </CardLocal>
+  );
   
   return (
       <Container>
@@ -84,13 +95,14 @@ const BloodBankInfoCard = ({ name, address, opening_hours }) => (
         <h4>Encontre um banco de sangue e faça a sua parte!</h4>
         <div className='mapInfo' >
           <div className='map' >
+          
               <GoogleMap
                 mapContainerStyle={{width: '100%', height: '100%'}}
                 center={{
-                  lat: location.latitude,
-                  lng: location.longitude
+                  lat: lat,
+                  lng: lng
                 }}
-                zoom={13}
+                zoom={18}
             
               >
                 {bloodBanks.map((bloodBank) => (
@@ -100,7 +112,11 @@ const BloodBankInfoCard = ({ name, address, opening_hours }) => (
                     lat: bloodBank.location.lat,
                     lng: bloodBank.location.lng
                   }}
-                  icon={<BloodtypeIcon />}
+                  title={bloodBank.name}
+                  onClick={() => {
+                    const url = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(bloodBank.name)}`;
+                    window.open(url, '_blank');
+                  }}
                 >
                   
                 </Marker>
@@ -109,12 +125,14 @@ const BloodBankInfoCard = ({ name, address, opening_hours }) => (
             </GoogleMap>
           </div>
           <Grid container spacing={2} sx={{mt: '2%'}}>
+            {console.log(bloodBanks)}
             {bloodBanks.map((bloodBank) => (
                 <Grid item xs={12} md={6} key={bloodBank.name}>
                   <BloodBankInfoCard
                   name={bloodBank.name}
                   address={bloodBank.address}
                   opening_hours={bloodBank.opening_hours}
+                  local = {bloodBank.location}
                 />
                 </Grid>
             ))}
--- client/src/components/Feed/LocalDoacao/styles.js
+++ client/src/components/Feed/LocalDoacao/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import * as palette from "../../../utils/variablesColor"
+import { Card } from '@mui/material'
 
 export const Container = styled.div`
      @media (max-width: 768px) {
@@ -48,4 +49,12 @@ export const Container = styled.div`
         
     }
     
+`
+
+
+
+export const CardLocal = styled(Card)`
+    .content:hover{
+        border: 0.3px solid rgba(204, 0, 0, 0.5);
+    }
 `
